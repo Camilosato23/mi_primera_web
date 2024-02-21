766c3ca63f14d44bf937c2d6ac6e9c7e55ac3c9e
Ivan Camilo Samaca Tovar <69828026+Camilosato23@users.noreply.github.com>
2024-02-20 19:09:30 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//alert("Hola este es mi javascript");
+
+let nombre = "Ivan";
+
+var nombre1 = "Ivan";
+
+const nombre2 = "Ivan";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
